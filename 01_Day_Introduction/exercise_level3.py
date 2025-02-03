8c4948f230514f9a08b9789ce83f4126c3081b76
Andrew552004 <afvanegas552004@gmail.com>
2025-02-02 22:08:26 -0500

    srcript to calculate euclidean distance was added

---
+++ 01_Day_Introduction/exercise_level3.py
@@ -0,0 +1,10 @@
+"""
+Description: This script calculates the Euclidean distance between two points in a 2D space.
+             The formula for Euclidean distance is: sqrt((x2 - x1)^2 + (y2 - y1)^2).
+
+Author: Andres Vanegas
+Date: 02/02/2025
+"""
+#Euclidean distance between two points (2,3) and (10,8)
+euclidean_distance = ((2-10)**2 + (3-8)**2)**0.5;
+print(f"Euclidean distance between (2,3) and (10,8) is: {euclidean_distance}");
