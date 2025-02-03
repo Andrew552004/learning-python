019b4ff066eb1ade6ae48c9d058bf6739c00c75c
Andrew552004 <afvanegas552004@gmail.com>
2025-02-02 21:37:35 -0500

    helloworld.py was added

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,36 @@
+"""
+Description: In this file, we will practice some basics of 
+python programming language.
+
+Author: Andres Vanegas
+Date: 02/02/2025
+"""
+
+# Basis c Operations
+print (f"Addition:{3 + 4}")
+print (f"Sustraction:{3 - 4}")
+print (f"Multiplication:{3 * 4}")
+print (f"Modulus:{3 % 4}")
+print (f"Division:{3 / 4}")
+print (f"Powers:{3 ** 4}")
+print (f"Floor division:{3 // 4}")
+
+# Inputs and prints
+name = input("What is your name? ")
+print(f"Your name is:{name}")
+
+family_name = input("What is your family name? ")
+print(f"Your family name is:{family_name}")
+
+country = input("where are you from? ")
+print(f"Your country is:{country}")
+
+what_ever = input("Are you enjoying 30 days of python?")
+print(f"Response:{what_ever}")
+
+# Verify the data type
+print(type(10));
+print(type(9.8));
+print(type(3.14)); 
+print(type(4-4j));
+print(type(['Andres', ' Vanegas', 'Colombia']));
